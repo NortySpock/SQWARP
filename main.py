eeac5f5997723875c507863d38edf7206de42282
David Norton <david.k.norton@gmail.com>
2014-04-27 11:17:15 -0500

    abstracted away basic ship drawing. Probably needs a lot more variables but works as demo for now

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import pygame, sys
 from pygame.locals import *
 
+def draw_ship(color, coord):
+    pygame.draw.polygon(DISPLAYSURF, color,((coord[0],coord[1]),(coord[0]+5,coord[1]+5),(coord[0],coord[1]-5),(coord[0]-5,coord[1]+5)))
+
 main_window_width = 500
 main_window_height = 400
 
 pygame.init()
+
+FPS = 30
+fpsClock = pygame.time.Clock()
+
 DISPLAYSURF = pygame.display.set_mode((main_window_width,main_window_height),0,32)
 pygame.display.set_caption('Drawing')
 
@@ -15,8 +22,8 @@ GREEN = (  0,255,  0)
 BLUE  = (  0,  0,255)
 
 DISPLAYSURF.fill(BLACK)
+draw_ship(GREEN,(250,200))
 
-pygame.draw.polygon(DISPLAYSURF, GREEN,((250,200),(245,210),(250,206),(255,210)))
 
 while True:
     for event in pygame.event.get():
@@ -24,3 +31,4 @@ while True:
             pygame.quit()
             sys.exit()
     pygame.display.update()
+
