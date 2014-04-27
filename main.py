3d50ca6ea7eba946d6d2683e88282379267b9465
David Norton <david.k.norton@gmail.com>
2014-04-27 10:40:14 -0500

    Now it draws a polygon

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import pygame, sys
 from pygame.locals import *
 
-main_window_width = 400
-main_window_height = 300
+main_window_width = 500
+main_window_height = 400
 
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((main_window_width,main_window_height))
-pygame.display.set_caption('Hello World!')
+DISPLAYSURF = pygame.display.set_mode((main_window_width,main_window_height),0,32)
+pygame.display.set_caption('Drawing')
+
+BLACK = (  0,  0,  0)
+WHITE = (255,255,255)
+RED   = (255,  0,  0)
+GREEN = (  0,255,  0)
+BLUE  = (  0,  0,255)
+
+DISPLAYSURF.fill(WHITE)
+
+pygame.draw.polygon(DISPLAYSURF, GREEN,((146,0),(291,106),(236,277),(56,277),(0,106)))
 
 while True:
     for event in pygame.event.get():
